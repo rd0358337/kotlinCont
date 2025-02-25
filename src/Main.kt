e92913b38351cc733a187d8217ee8649cb49e460
rd0358337 <rd0358337@otc.edu>
2025-02-24 23:18:23 -0600

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,7 @@
 class Snake(var snakeName: String?, var isVenomous: Boolean) {
 
     init {
-
+        //output for snake name
         println("Snake details have been recorded: $snakeName")
     }
 }
@@ -9,21 +9,23 @@ class Snake(var snakeName: String?, var isVenomous: Boolean) {
 fun main() {
 
     val snakeList = mutableListOf("Cobra", "Viper", "Python", "Boa")
-
+    //loop to allow for multiple snake entries
     while (true) {
 
         print("Enter a species of snake: ")
         val snakeSpecies: String? = readLine()
-
+        //input validation
         if (snakeSpecies == null) {
             println("You did not enter any information")
             continue
         }
 
-        print("Is this snake venomous? (yes/no)")
+        print("Is this snake venomous? (yes/no): ")
         val isVenomous: String? = readLine()
+        //creates snake object
+        Snake(snakeSpecies, isVenomous == "yes")
 
-
+        //adds snake to list
         snakeList.add(snakeSpecies)
 
         println("Would you like to add another snake? (yes/no)")
@@ -31,7 +33,7 @@ fun main() {
         if (addSnake != "yes")
             break
     }
-
+    //displays finalized snake list
     println("Current snakes list: ")
     for (snake in snakeList) {
         println("- $snake")
